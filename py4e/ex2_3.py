05cec5d75d72355b1689d62ff9fa4708d3e8beed
sbj6364 <sbj6364@daum.net>
2021-01-26 15:50:42 +0900

    add 'ex2_3.py'

---
+++ py4e/ex2_3.py
@@ -0,0 +1,8 @@
+#2.3 Gross pay calculator
+
+hours = input("Please enter the hours you worked: ")
+rate = input("What is the pay rate per hour?: ")
+
+pay = float(hours)*float(rate)
+
+print("Then you deserve",pay,"$")
