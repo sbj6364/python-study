06aa24b8fd997fb573ff3601f378a8264a1d2caa
Seo Byeong Jun <sbj6364@kakao.com>
2021-02-17 14:13:13 +0900

    Overtime calculater

---
+++ py4e/ex3_1.py
@@ -0,0 +1,11 @@
+sh = input("Enter Hours: ")
+sr = input("Enter Rate: ")
+fh = float(sh)
+fr = float(sr)
+# print(fh, fr)
+if fh > 40 :
+	print("Overtime")
+else:
+	print("Regular")
+xp = fh * fr
+print("Pay:",xp)
