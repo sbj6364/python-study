95c7ebfe41fb904770148072cd983889bd53c1c1
Seo Byeong Jun <sbj6364@kakao.com>
2021-02-17 14:17:55 +0900

    Add pay calculater

--- py4e/ex3_1.py
+++ py4e/ex3_1.py
@@ -5,7 +5,11 @@ fr = float(sr)
 # print(fh, fr)
 if fh > 40 :
 	print("Overtime")
+	reg = fh * fr
+	otp = (fh - 40.0) * (fr * 0.5)
+	# print(reg, otp)
+	xp = reg + otp
 else:
 	print("Regular")
-xp = fh * fr
+	xp = fh * fr
 print("Pay:",xp)
