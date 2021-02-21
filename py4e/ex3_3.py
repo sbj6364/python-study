96fa672e736c4d44835a094101a16587b690682a
Seo Byeong Jun <sbj6364@kakao.com>
2021-02-21 22:47:56 +0900

    Add grade calculator

---
+++ py4e/ex3_3.py
@@ -0,0 +1,12 @@
+score = float(input("Enter Score: "))
+if score < 0.6:
+    grade = 'F'
+elif score < 0.7:
+    grade = 'D'
+elif score < 0.8:
+    grade = 'C'
+elif score < 0.9:
+    grade = 'B'
+elif score <= 1.0:
+    grade = 'A'
+print(grade)
