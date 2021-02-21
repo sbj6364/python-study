64fdb49877114d80f218b76f55d883316c5d03dc
Seo Byeong Jun <sbj6364@kakao.com>
2021-02-21 17:56:03 +0900

    Edit .gitignore

--- py4e/ex3_2.py
+++ py4e/ex3_2.py
@@ -7,9 +7,9 @@ except:
     print("Error, please enter numeric input")
     quit()
 if fh > 40:
-	reg = fh * fr
-	otp = (fh - 40.0) * (fr * 0.5)
-	xp = reg + otp
+    reg = fh * fr
+    otp = (fh - 40.0) * (fr * 0.5)
+    xp = reg + otp
 else:
-	xp = fh * fr
+    xp = fh * fr
 print("Pay:", xp)
