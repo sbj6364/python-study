183961b4559de3f5ac3c11598dbd59843837fdf2
Seo Byeong Jun <sbj6364@kakao.com>
2021-02-26 17:03:28 +0900

    Add average calculator & user interface

--- py4e/ex7_2.py
+++ py4e/ex7_2.py
@@ -1,8 +1,13 @@
 fname = input("Enter file name: ")
 fh = open(fname)
-
+cnt = 0
+value = 0
 for line in fh:
     if not line.startswith("X-DSPAM-Confidence:"):
         continue
-    print(line)
-print("Done")
+    cnt = cnt + 1
+    i = line.find(':')
+    num = float(line[i+2:])
+    value += num
+ave = value / cnt
+print("Average spam confidence:", ave)
