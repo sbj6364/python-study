99424208d15209dee74e41d33628e8c4bbea3101
Seo Byeong Jun <sbj6364@kakao.com>
2021-03-07 01:37:54 +0900

    Create new directory 'futureskill'

---
+++ futureskill/barista.py
@@ -0,0 +1,11 @@
+coffee = 4100
+juice = 4600
+blacktea = 3900
+
+ord1 = (3*coffee) + (2*juice) + blacktea
+ord2 = (4*coffee) + (3*juice) + (3*blacktea)
+ord3 = coffee + juice + (2*blacktea)
+
+print("order 1 total =", ord1)
+print("order 2 total =", ord2)
+print("order 3 total =", ord3)
