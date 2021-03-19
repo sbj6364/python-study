55e846b91615225290ba73b775064948ea20d3e3
Seo Byeong Jun <sbj6364@kakao.com>
2021-03-19 17:10:35 +0900

    Create listzip.py

---
+++ futureskill/listzip.py
@@ -0,0 +1,21 @@
+def swap(a): # swap index
+    tmp = a[0]
+    a[0] = a[1]
+    a[1] = tmp
+
+tmp = input("a = ")
+a = tmp.split()
+tmp = input("b = ")
+b = tmp.split()
+ans = []
+t = [] # temporary
+
+for i in range(4): # move to answer list
+    t.append(a[i])
+    t.append(b[i])
+    ans.append(t)
+    t = []
+
+swap(ans[1])
+swap(ans[3])
+print(ans)
